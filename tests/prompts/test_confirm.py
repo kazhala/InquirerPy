9918af02a87492c1ed232b5ffd68ce1b3727d6f8
Kevin Zhuang <kevin7441@gmail.com>
2020-11-07 22:26:32 +1100

    test(confirm): init test for confirm prompt

--- tests/prompts/test_confirm.py
+++ tests/prompts/test_confirm.py
@@ -1,7 +1,5 @@
-import io
-import sys
 import unittest
-from unittest.mock import patch
+from unittest.mock import ANY, patch
 
 from prompt_toolkit.input.defaults import create_pipe_input
 from prompt_toolkit.keys import Keys
@@ -17,7 +15,7 @@ class TestConfirmPrompt(unittest.TestCase):
     def tearDown(self):
         self.inp.close()
 
-    def test_confirm_prompt(self):
+    def test_default_false(self):
         self.inp.send_text("\n")
         confirm_prompt = Confirm(
             message="hello",
@@ -27,5 +25,156 @@ class TestConfirmPrompt(unittest.TestCase):
             output=DummyOutput(),
             input=self.inp,
         )
+        result = confirm_prompt.execute()
+        self.assertEqual(result, False)
+        self.assertEqual(confirm_prompt.status["answered"], True)
+        self.assertEqual(confirm_prompt.status["result"], False)
+
+    def test_default_true(self):
+        self.inp.send_text("\n")
+        confirm_prompt = Confirm(
+            message="hello",
+            style={"symbol": "bold", "answer": "#000000"},
+            default_true=True,
+            symbol="x",
+            output=DummyOutput(),
+            input=self.inp,
+        )
+        result = confirm_prompt.execute()
+        self.assertEqual(result, True)
+        self.assertEqual(confirm_prompt.status["answered"], True)
+        self.assertEqual(confirm_prompt.status["result"], True)
+
+    def test_input_y(self):
+        self.inp.send_text("y")
+        confirm_prompt = Confirm(
+            message="hello",
+            style={},
+            default_true=True,
+            symbol="?",
+            output=DummyOutput(),
+            input=self.inp,
+        )
         result = confirm_prompt.session.prompt()
-        self.assertEqual(result, "")
+        self.assertEqual(result, True)
+        self.assertEqual(confirm_prompt.status["answered"], True)
+        self.assertEqual(confirm_prompt.status["result"], True)
+
+        self.inp.send_text("Y")
+        confirm_prompt = Confirm(
+            message="hello",
+            style={},
+            default_true=True,
+            symbol="?",
+            output=DummyOutput(),
+            input=self.inp,
+        )
+        result = confirm_prompt.execute()
+        self.assertEqual(result, True)
+
+    def test_input_n(self):
+        self.inp.send_text("n")
+        confirm_prompt = Confirm(
+            message="hello",
+            style={},
+            default_true=True,
+            symbol="?",
+            output=DummyOutput(),
+            input=self.inp,
+        )
+        result = confirm_prompt.execute()
+        self.assertEqual(result, False)
+        self.assertEqual(confirm_prompt.status["answered"], True)
+        self.assertEqual(confirm_prompt.status["result"], False)
+
+        self.inp.send_text("N")
+        confirm_prompt = Confirm(
+            message="hello",
+            style={},
+            default_true=True,
+            symbol="?",
+            output=DummyOutput(),
+            input=self.inp,
+        )
+        result = confirm_prompt.execute()
+        self.assertEqual(result, False)
+
+    def test_get_prompt_message(self):
+        confirm_prompt = Confirm(
+            message="hello",
+            style={},
+            default_true=True,
+            symbol="?",
+        )
+        message = confirm_prompt.get_prompt_message()
+        self.assertEqual(
+            message,
+            [
+                ("class:symbol", "?"),
+                ("class:question", " hello "),
+                ("class:instruction", " (Y/n)"),
+            ],
+        )
+
+        confirm_prompt.status["answered"] = True
+        confirm_prompt.status["result"] = True
+        message = confirm_prompt.get_prompt_message()
+        self.assertEqual(
+            message,
+            [
+                ("class:symbol", "?"),
+                ("class:question", " hello "),
+                ("class:answer", " Yes"),
+            ],
+        )
+
+        confirm_prompt = Confirm(
+            message="Are you sure?",
+            style={},
+            default_true=False,
+            symbol="x",
+        )
+        message = confirm_prompt.get_prompt_message()
+        self.assertEqual(
+            message,
+            [
+                ("class:symbol", "x"),
+                ("class:question", " Are you sure? "),
+                ("class:instruction", " (y/N)"),
+            ],
+        )
+
+        confirm_prompt.status["answered"] = True
+        confirm_prompt.status["result"] = False
+        message = confirm_prompt.get_prompt_message()
+        self.assertEqual(
+            message,
+            [
+                ("class:symbol", "x"),
+                ("class:question", " Are you sure? "),
+                ("class:answer", " No"),
+            ],
+        )
+
+    @patch("InquirerPy.prompts.confirm.Confirm.get_prompt_message")
+    @patch("InquirerPy.prompts.confirm.Style.from_dict")
+    @patch("InquirerPy.prompts.confirm.KeyBindings")
+    @patch("InquirerPy.prompts.confirm.PromptSession")
+    def test_callable_called(
+        self, MockedSession, MockedKeyBindings, MockedStyle, mocked_message
+    ):
+        prompt = Confirm(
+            message="Are you sure?",
+            style={},
+            default_true=False,
+            symbol="x",
+        )
+        kb = MockedKeyBindings()
+        style = MockedStyle()
+        MockedSession.assert_called_once_with(
+            message=mocked_message,
+            key_bindings=kb,
+            style=style,
+            input=None,
+            output=None,
+        )
