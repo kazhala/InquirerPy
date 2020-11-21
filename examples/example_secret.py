84ac923fea6088a1a8072b6a431ef524b65b616a
Kevin Zhuang <kevin7441@gmail.com>
2020-11-21 16:19:03 +1100

    refactor(examples): move examples

--- examples/example_secret.py
+++ examples/example_secret.py
@@ -2,10 +2,7 @@ from InquirerPy.resolver import prompt
 from InquirerPy.validator import EmptyInputValidator
 
 questions = [
-    {
-        "type": "secret",
-        "question": "Enter your password",
-    },
+    {"type": "secret", "question": "Enter your password", "default": "1"},
     {
         "type": "secret",
         "question": "Enter secret",
