ad0e6147aff40da9cd136cb17ead6581eda2521c
Kevin Zhuang <kevin7441@gmail.com>
2020-12-06 14:00:02 +1100

    chore(example): added rawlist example

---
+++ examples/example_rawlist.py
@@ -0,0 +1,19 @@
+from InquirerPy.resolver import prompt
+from InquirerPy.separator import Separator
+
+questions = [
+    {
+        "type": "rawlist",
+        "options": ["hello", "world", "foo", "boo"],
+        "question": "Select one of them",
+        "default": "boo",
+    },
+    {
+        "type": "rawlist",
+        "options": [{"name": "foo", "value": "boo"}, "hello", Separator(), "yes"],
+        "question": "Select things apply",
+    },
+]
+
+result = prompt(questions, editing_mode="vim")
+print(result)
