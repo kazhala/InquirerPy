f95d7da50d6d05b4d1c5817012eaa4aec0e28016
Kevin Zhuang <kevin7441@gmail.com>
2020-12-11 11:43:42 +1100

    feat(expand): setup keybinding_factory

--- InquirerPy/prompts/expand.py
+++ InquirerPy/prompts/expand.py
@@ -1,7 +1,8 @@
 """Module contains the expand prompt and its related helper classes."""
-from typing import Any, Dict, List, Tuple, Union
+from typing import Any, Dict, List, Literal, Tuple, Union
 
 from InquirerPy.base import BaseComplexPrompt, InquirerPyUIControl
+from InquirerPy.exceptions import InvalidArgument, RequiredKeyNotFound
 from InquirerPy.separator import Separator
 
 
@@ -20,7 +21,7 @@ class InquirerPyExpandControl(InquirerPyUIControl):
 
     def __init__(
         self,
-        options: List[Union[Any, Dict[str, Any]]],
+        options: List[Dict[str, Any]],
         default: str = None,
         pointer: str = " ",
         separator: str = ")",
@@ -28,10 +29,47 @@ class InquirerPyExpandControl(InquirerPyUIControl):
         """Construct UIControl object and initialise options."""
         self.pointer = "%s " % pointer
         self.separator = separator
+        self.expanded = False
+        self.key_maps = {"h": -1}
         super().__init__(options, default)
 
-        for raw_option, option in zip(options, self.options):
-            option["key"] = raw_option["key"]
+        try:
+            count = 0
+            separator_count = 0
+            for raw_option, option in zip(options, self.options):
+                if not isinstance(raw_option, dict):
+                    raise InvalidArgument(
+                        "expand type prompt require each option to be a dictionary."
+                    )
+                if isinstance(option["value"], Separator):
+                    separator_count += 1
+                else:
+                    option["key"] = raw_option["key"]
+                    self.key_maps[option["key"]] = count
+                count += 1
+        except KeyError:
+            raise RequiredKeyNotFound(
+                "each option require the dictionary key 'key' to be present."
+            )
+
+    def _get_formatted_options(self) -> List[Tuple[str, str]]:
+        """Override this parent class method as expand require visual switch of content.
+
+        1. non expand mode
+        2. expand mode
+
+        :return: a list of formatted options
+        :rtype: List[Tuple[str, str]]
+        """
+        if self.expanded:
+            return super()._get_formatted_options()
+        else:
+            display_choices = []
+            display_choices.append(("class:pointer", ">> "))
+            display_choices.append(
+                ("", self.options[self.selected_option_index]["value"])
+            )
+        return display_choices
 
     def _get_hover_text(self, option) -> List[Tuple[str, str]]:
         display_message = []
@@ -50,3 +88,38 @@ class InquirerPyExpandControl(InquirerPyUIControl):
             display_message.append(("", "%s%s " % (option["key"], self.separator)))
         display_message.append(("", option["name"]))
         return display_message
+
+    def _set_selected_option(self, key: str) -> None:
+        self.selected_option_index = self.key_maps[key]
+
+
+class ExpandPrompt(BaseComplexPrompt):
+    """Create a `prompt_toolkit` application and responsible to render the expand prompt.
+
+    :param options: list of options to display
+    :type options: List[Dict[str, Any]]
+    """
+
+    def __init__(
+        self,
+        message: str,
+        options: List[Dict[str, Any]],
+        default: str,
+        style: Dict[str, str] = {},
+        editing_mode: Literal["default", "emacs", "vim"] = "default",
+        symbol: str = "?",
+        pointer: str = " ",
+        separator: str = ")",
+    ) -> None:
+        """Create the application and apply keybindings."""
+        self.content_control: InquirerPyExpandControl = InquirerPyExpandControl(
+            options, default, pointer, separator
+        )
+        super().__init__(message, style, editing_mode, symbol)
+
+        def keybinding_factory(key):
+            @self.kb.add(key)
+            def keybinding(_) -> None:
+                self.content_control.selected_option_index = (
+                    self.content_control.key_maps[key]
+                )
