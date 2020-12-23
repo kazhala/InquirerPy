3c8355a746068783956772cc1fbc23da64487312
Kevin Zhuang <kevin7441@gmail.com>
2020-12-23 15:04:11 +1100

    chore(example): added fuzzy example

--- examples/example_fuzzy.py
+++ examples/example_fuzzy.py
@@ -1,23 +1,43 @@
+from contextlib import ExitStack
 from pathlib import Path
+import urllib.request
 
 from InquirerPy.resolver import prompt
 
-p = Path("~/Programming/shell/dotbare/tests").expanduser()
-paths = []
-for path in p.rglob("*"):
-    paths.append(path)
-paths = paths * 20
+p = Path(__file__).resolve().parent.joinpath("sample.txt")
+choices = []
+
+with ExitStack() as stack:
+    if not p.exists():
+        print("hello")
+        file = stack.enter_context(p.open("w+"))
+        sample = stack.enter_context(
+            urllib.request.urlopen(
+                "https://kazhala-public-shit.s3-ap-southeast-2.amazonaws.com/sample.txt"
+            )
+        )
+        file.write(sample.read().decode())
+        file.seek(0, 0)
+    else:
+        file = stack.enter_context(p.open("r"))
+    for line in file.readlines():
+        choices.append(line[:-1])
 
 questions = [
     {
         "type": "fuzzy",
         "message": "Select one of them",
-        "choices": paths,
+        "choices": choices,
         "border": True,
         "multiselect": True,
         "max_height": "100%",
         "validate": lambda x: len(x) > 1,
-    }
+    },
+    {
+        "type": "fuzzy",
+        "message": "Select one",
+        "choices": ["hello", "weather", "what", "whoa"],
+    },
 ]
 
 result = prompt(questions=questions, editing_mode="vim")
