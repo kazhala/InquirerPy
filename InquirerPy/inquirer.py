d710f376fc519c398e0be1212f090a6a9de683f7
Kevin Zhuang <kevin7441@gmail.com>
2020-12-30 12:32:34 +1100

    refactor: update import

--- InquirerPy/inquirer.py
+++ InquirerPy/inquirer.py
@@ -3,12 +3,14 @@
 Servers as another entrypoint of constructing a prompt providing
 proper type hinting and completions.
 """
-from InquirerPy.prompts.checkbox import CheckboxPrompt as checkbox
-from InquirerPy.prompts.confirm import ConfirmPrompt as confirm
-from InquirerPy.prompts.expand import ExpandPrompt as expand
-from InquirerPy.prompts.filepath import FilePathPrompt as filepath
-from InquirerPy.prompts.fuzzy.fuzzy import FuzzyPrompt as fuzzy
-from InquirerPy.prompts.input import InputPrompt as text
-from InquirerPy.prompts.list import ListPrompt as select
-from InquirerPy.prompts.rawlist import RawlistPrompt as rawlist
-from InquirerPy.prompts.secret import SecretPrompt as secret
+from InquirerPy.prompts import (
+    CheckboxPrompt as checkbox,
+    ConfirmPrompt as confirm,
+    ExpandPrompt as expand,
+    FilePathPrompt as filepath,
+    FuzzyPrompt as fuzzy,
+    InputPrompt as text,
+    ListPrompt as select,
+    RawlistPrompt as rawlist,
+    SecretPrompt as secret,
+)
---
+++ InquirerPy/prompts/__init__.py
@@ -0,0 +1,10 @@
+"""Module contains import of all prompts classes."""
+from InquirerPy.prompts.checkbox import CheckboxPrompt
+from InquirerPy.prompts.confirm import ConfirmPrompt
+from InquirerPy.prompts.expand import ExpandPrompt
+from InquirerPy.prompts.filepath import FilePathPrompt
+from InquirerPy.prompts.fuzzy.fuzzy import FuzzyPrompt
+from InquirerPy.prompts.input import InputPrompt
+from InquirerPy.prompts.list import ListPrompt
+from InquirerPy.prompts.rawlist import RawlistPrompt
+from InquirerPy.prompts.secret import SecretPrompt
