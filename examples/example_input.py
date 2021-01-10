8827ac003ac4eef611fec3350bc3cb79ae09e82a
Kevin Zhuang <kevin7441@gmail.com>
2021-01-10 18:17:13 +1100

    chore(example): update input example

--- examples/example_input.py
+++ examples/example_input.py
@@ -1,27 +1,59 @@
-from InquirerPy.resolver import prompt
+from InquirerPy import inquirer, prompt
+from InquirerPy.validator import NumberValidator
 
-questions = [
-    {"type": "input", "message": "Enter your name"},
-    {
-        "type": "input",
-        "message": "What do you want",
-        "completer": {
-            "hello": None,
-            "what": None,
-            "hey": None,
-            "whaa": None,
-            "whaaa": None,
-            "wheee": None,
+
+def classic():
+    """Classic syntax example."""
+    questions = [
+        {"type": "input", "message": "Enter your name:"},
+        {
+            "type": "input",
+            "message": "Which company would you like to apply:",
+            "completer": {
+                "Google": None,
+                "Facebook": None,
+                "Amazon": None,
+                "Netflix": None,
+                "Apple": None,
+                "Microsoft": None,
+            },
+            "multicolumn_complete": True,
         },
-        "multicolumn_complete": True,
-    },
-    {
-        "type": "input",
-        "message": "What do you want",
-        "multiline": True,
-        "filter": lambda x: x * 2,
-    },
-]
+        {
+            "type": "input",
+            "message": "What's your salary expectation(k):",
+            "transformer": lambda result: "%sk" % result,
+            "filter": lambda result: int(result) * 1000,
+            "validate": NumberValidator(),
+        },
+    ]
 
-result = prompt(questions, vi_mode=True)
-print(result)
+    result = prompt(questions)
+
+
+def alternate():
+    """Alternate syntax example."""
+
+    name = inquirer.text(message="Enter your name:").execute()
+    company = inquirer.text(
+        message="Which company would you like to apply:",
+        completer={
+            "Google": None,
+            "Facebook": None,
+            "Amazon": None,
+            "Netflix": None,
+            "Apple": None,
+            "Microsoft": None,
+        },
+        multicolumn_complete=True,
+    ).execute()
+    salary = inquirer.text(
+        message="What's your salary expectation(k):",
+        transformer=lambda result: "%sk" % result,
+        filter=lambda result: int(result) * 1000,
+        validate=NumberValidator(),
+    ).execute()
+
+
+# alternate()
+classic()
