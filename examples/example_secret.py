6bcfa797e8cb18bee558059cda4d0a543a4dfe77
Kevin Zhuang <kevin7441@gmail.com>
2021-01-11 12:45:47 +1100

    chore(example): secret example update

--- examples/example_secret.py
+++ examples/example_secret.py
@@ -1,27 +1,46 @@
-from InquirerPy.resolver import prompt
-from InquirerPy.validator import EmptyInputValidator, PasswordValidator
+from InquirerPy import prompt, inquirer
+from InquirerPy.validator import PasswordValidator
 
-questions = [
-    {
-        "type": "secret",
-        "message": "Enter your password",
-        "default": "1",
-        "transformer": lambda x: x * 2,
-    },
-    {
-        "type": "secret",
-        "message": "Enter secret",
-        "name": "secret_key",
-        "qmark": "*",
-        "validate": EmptyInputValidator(),
-    },
-    {
-        "type": "secret",
-        "message": "New password",
-        "name": "password",
-        "validate": PasswordValidator(length=8, cap=True, special=True, number=True),
-    },
-]
-result = prompt(questions, vi_mode=True)
+original_password = "InquirerPy45@"
 
-print(result)
+
+def classic():
+    questions = [
+        {
+            "type": "password",
+            "message": "Old password:",
+            "transformer": lambda _: "[hidden]",
+            "validate": lambda text: text == original_password,
+            "invalid_message": "Wrong password",
+        },
+        {
+            "type": "password",
+            "message": "New password:",
+            "name": "new_password",
+            "validate": PasswordValidator(
+                length=8, cap=True, special=True, number=True
+            ),
+            "transformer": lambda _: "[hidden]",
+        },
+        {"type": "confirm", "message": "Confirm?", "default": True},
+    ]
+    result = prompt(questions)
+
+
+def alternate():
+    old_password = inquirer.secret(
+        message="Old password:",
+        transformer=lambda _: "[hidden]",
+        validate=lambda text: text == original_password,
+        invalid_message="Wrong password",
+    ).execute()
+    new_password = inquirer.secret(
+        message="New password:",
+        validate=PasswordValidator(length=8, cap=True, special=True, number=True),
+        transformer=lambda _: "[hidden]",
+    ).execute()
+    confirm = inquirer.confirm(message="Confirm?", default=True).execute()
+
+
+alternate()
+# classic()
