845bac731a78258b6befe50c7a1a3ae656b87331
Kevin Zhuang <kevin7441@gmail.com>
2021-01-11 16:33:58 +1100

    chore(example): update confirm example

--- examples/example_confirm.py
+++ examples/example_confirm.py
@@ -1,19 +1,37 @@
-from InquirerPy import prompt
+from InquirerPy import prompt, inquirer
 
-questions = [
-    {
-        "type": "confirm",
-        "name": "hello",
-        "message": "Proceed?",
-        "default": True,
-        "qmark": "?",
-    },
-    {
-        "type": "confirm",
-        "name": "foo",
-        "message": "Are you sure?",
-        "when": lambda result: result["hello"] == True,
-    },
-]
 
-print(prompt(questions))
+def classic():
+    questions = [
+        {
+            "type": "confirm",
+            "message": "Proceed?",
+            "name": "proceed",
+            "default": True,
+        },
+        {
+            "type": "confirm",
+            "message": "Require 1 on 1?",
+            "when": lambda result: result["proceed"],
+        },
+        {
+            "type": "confirm",
+            "message": "Confirm?",
+            "when": lambda result: result.get("1", False),
+        },
+    ]
+
+    result = prompt(questions)
+
+
+def alternate():
+    proceed, service, confirm = False, False, False
+    proceed = inquirer.confirm(message="Proceed?", default=True).execute()
+    if proceed:
+        service = inquirer.confirm(message="Require 1 on 1?").execute()
+    if service:
+        confirm = inquirer.confirm(message="Confirm?").execute()
+
+
+alternate()
+# classic()
