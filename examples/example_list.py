1cd49670ca9c91988eef0f0c9833cceda90823ac
Kevin Zhuang <kevin7441@gmail.com>
2021-01-12 19:18:48 +1100

    chore(example): update list example

--- examples/example_list.py
+++ examples/example_list.py
@@ -1,27 +1,33 @@
 from InquirerPy.resolver import prompt
 from InquirerPy.separator import Separator
 
-questions = [
-    {
-        "type": "list",
-        "message": "Select a fruit:",
-        "choices": [{"name": "banana", "value": "peach"}, "apple", "orange"],
-        "default": "apple",
-        "multiselect": True,
-    },
-    {
-        "type": "list",
-        "message": "Select a sport:",
-        "choices": [
-            {"name": "Basketball", "value": "NBA"},
-            {"name": "Rugby", "value": "NFL"},
-            Separator(),
-            "Soccer",
-        ],
-        "default": "apple",
-        "filter": lambda x: "%s888888" % x,
-    },
-]
 
-result = prompt(questions=questions, vi_mode=True)
-print(result)
+def classic():
+    questions = [
+        {
+            "type": "list",
+            "message": "Select an action:",
+            "choices": ["Upload", "Download", {"name": "Exit", "value": None}],
+            "default": None,
+        },
+        {
+            "type": "list",
+            "message": "Select regions:",
+            "choices": [
+                {"name": "Sydney", "value": "ap-southeast-2"},
+                {"name": "Singapore", "value": "ap-southeast-1"},
+                Separator(),
+                "us-east-1",
+                "us-east-2",
+            ],
+            "multiselect": True,
+            "transformer": lambda result: "%s region%s selected"
+            % (len(result), "s" if len(result) > 1 else ""),
+            "when": lambda result: result["0"] is not None,
+        },
+    ]
+
+    result = prompt(questions=questions)
+
+
+classic()
