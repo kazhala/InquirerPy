b14ccbb042c7bae43abdd3a23f3b009963a4ad9e
Kevin Zhuang <kevin7441@gmail.com>
2021-07-03 19:51:25 +1000

    v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="InquirerPy",
-    version="0.2.0",
+    version="0.2.1",
     author="Kevin Zhuang",
     author_email="kevin7441@gmail.com",
     description="Enhaced Python port of Inquirer.js",
